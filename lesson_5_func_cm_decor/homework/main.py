422f0bd1d527d249161054d183746712ef0d5f8e
Maksym <rudyakmaxl@gmail.com>
2022-06-10 16:01:38 +0300

    Add file

---
+++ lesson_5_func_cm_decor/homework/main.py
@@ -0,0 +1,70 @@
+team: list[dict] = [
+    {"name": "John", "age": 20, "number": 1},
+    {"name": "Mark", "age": 33, "number": 3},
+    {"name": "Cavin", "age": 17, "number": 12},
+]
+
+
+def repr_players(players: list[dict], sorter: bool, key=lambda x: x["number"]) -> None:
+    print("TEAM:")
+    print(type(key))
+    if sorter:
+        for player in sorted(players, key=key):
+            print(
+                f"\t{player['number']} " f"Name: {player['name']}, Age: {player['age']}"
+            )
+    else:
+        for player in players:
+            print(
+                f"\t{player['number']} " f"Name: {player['name']}, Age: {player['age']}"
+            )
+    print("\n")
+
+
+def log(message: str) -> None:
+    print(f"-> -> -> {message} <- <- <-")
+
+
+def add_player(num: int, name: str, age: int) -> None:
+    player = {"name": name, "number": num, "age": age}
+    numer = False
+    for nums in team:
+        if nums["number"] == player["number"]:
+            numer = True
+    if numer is True:
+        print(log(message="You can't add this player"))
+    else:
+        team.append(player)
+        log(message=f"Adding {player['name']}")
+
+
+def remove_player(players: list[dict], num: int) -> None:
+    for index, player in enumerate(players):
+        if player["number"] == num:
+            player_name = player["name"]
+            del players[index]
+            log(message=f"Deleting {player_name}")
+
+
+def update_player(num: int) -> None:
+    for player in team:
+        if player["number"] == num:
+            nums = int(input("Enter a new player number: "))
+            player["number"] = nums
+            name = input("Enter a new player name: ")
+            player["name"] = name
+            age = input("Enter a new player age: ")
+            player["age"] = age
+
+
+# =====================================================
+def main():
+    pass
+
+
+# =====================================================
+
+if __name__ == "__main__":
+    main()
+else:
+    raise SystemExit("This module in only for running")
